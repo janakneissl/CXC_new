313c8d449838833e097ea636b1bdb6bec6357e27
janakneissl <jany95@outlook.com>
2016-08-18 15:10:57 +0900

    Update coverage.py

--- coverage.py
+++ coverage.py
@@ -1,11 +1,6 @@
 #!usr/bin/python
 
-###################
-# Get coverage from bam file:
-# chr1 pos1 chr2 pos2
-###################
-
-# command: python 01.py path/to/bam/file.bam
+# command: python coverage.py path/to/bam/file.bam
 # input: path to bamfile (bai file should be in same folder)
 
 import pysam
@@ -31,36 +26,45 @@ os.system('samtools index CXC_output/discordant.bam')
 #the discordant read bam file is read in 
 bamfile = pysam.AlignmentFile("CXC_output/discordant.bam", "rb")
 
+
+
 chromosomes = ['1','2','3','4','5','6','7','8','9','10','11', '12', '13', '14', '15', 
-				'16', '17', '18', '19', '20', '21', '22', 'X', 'Y', 'M']
+                '16', '17', '18', '19', '20', '21', '22', 'X', 'Y']
 
 rep = {'X':'23', 'Y':'24', 'M':'25'}
 rep = dict(rep.iteritems())
 pattern = re.compile("|".join(rep.keys()))
 
 for i in chromosomes: #for each chromosome the coverage information is saved in its own file
-	chr = "chr" + i
-	filename = 'CXC_output/'+ chr + '.txt'
-	f = open(filename,'w')
+    chr = "chr" + i
+    filename = 'CXC_output/'+ chr + '.txt'
+    f = open(filename,'w')
 
-	for column in bamfile.pileup(chr): #Loops through every position of the current chromosome
-		#then get the information for all the reads at this position and their mate reads
-		cov = bamfile.count_coverage(chr, column.pos, column.pos)
-		print cov 
-		cov_ar = np.zeros((cov,4))
-		read_nr = 0
+    for column in bamfile.pileup(chr): #Loops through every position of the current chromosome
+        cov = column.n
+        cov_ar = np.zeros((cov,4), dtype = int)
+        read_nr = 0
+        i = pattern.sub(lambda m:rep[re.escape(m.group(0))],i)
+        for read in column.pileups:
+            cov_ar[read_nr,0] = i
+            cov_ar[read_nr,1] = column.pos
+            cov_ar[read_nr,2] = pattern.sub(lambda m: rep[re.escape(m.group(0))],list(read.alignment.next_reference_name)[3])
+            cov_ar[read_nr,3] = read.alignment.next_reference_start
+            read_nr += 1
 
-		i = pattern.sub(lambda m:rep[re.escape(m.group(0))],i)
-		for read in column.pileups:
-			cov_ar[read_nr,0] = i
-			cov_ar[read_nr,1] = column.pos
-			cov_ar[read_nr,2] = pattern.sub(lambda m: rep[re.escape(m.group(0))],str(list(read.alignment.next_reference_name)[3]))
-			cov_ar[read_nr,3] = read.alignment.next_reference_start
-			read_nr += 1
+        #filter out correct reads (somehow there are still some with mate on same chromosome)
+        if len(cov_ar >=1):
+            cov_ar = cov_ar[cov_ar[:,0]!= cov_ar[:,2]]
+            chr_ar = np.bincount(cov_ar[:,2])
+            if len(cov_ar >=1):
+                percen = np.max(chr_ar)/float(len(cov_ar))
+                if percen >= 0.9 :
+                    real_reads = cov_ar[cov_ar[:,2] == np.argmax(chr_ar)]
+                    string = ''
+                    for read in real_reads:
+                        line = '\t'.join(np.array(read, dtype=str))
+                        string = string + line + '\n'
+                    f.write(string)
 
-		chr_ar = np.bincount(cov_ar)
-		if np.max(chr_ar)/cov >= 0.9:
-			real_reads = cov_ar[cov_ar[:,2] == np.argmax(chr_ar)]
-			outfile.write(real_reads)
-
-	f.close()
+               
+    f.close()
